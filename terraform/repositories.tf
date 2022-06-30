9e63c5d3a8bf860c126433c086c6771ccbd47efc
Stephen Kaye <Stephen.Kaye@digital.homeoffice.gov.uk>
2022-06-30 12:18:58 +0100

    Add build check to callisto-build-github

--- terraform/repositories.tf
+++ terraform/repositories.tf
@@ -16,7 +16,9 @@ locals {
     "callisto-test-2" : {
       "checks" : concat(local.checks.drone, local.checks.code_analysis)
     },
-    "callisto-build-github" : { }
+    "callisto-build-github" : { 
+        "checks": ["terraform-validate" ]
+    }
   }
   repository_configs = {
     for k, v in local.repositories : k => {
